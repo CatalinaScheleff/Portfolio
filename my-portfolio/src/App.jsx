b66075e5508beb7ebbbb2bda3d362b3c9fe0bea8
CatalinaScheleff <catascheleff@gmail.com>
2024-01-28 21:20:17 -0300

    start

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter} from "react-router-dom";
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { BackgroundCon } from "./components/AppElements";
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <BackgroundCon id="BackgroundCon">
+
+      </BackgroundCon>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ my-portfolio/src/components/AppElements.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const BackgroundCon = styled.div`
+background-color: #242424;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
