ea70be8350d7f94b23f65b2fca2d7b223b844510
CatalinaScheleff <catascheleff@gmail.com>
2024-01-30 20:57:34 -0300

    links working

--- my-portfolio/src/components/contactForm/ContactFormElements.jsx
+++ my-portfolio/src/components/contactForm/ContactFormElements.jsx
@@ -19,3 +19,4 @@ export const FormCon = styled.div`
 
 
 
+
--- my-portfolio/src/pages/contact.jsx
+++ my-portfolio/src/pages/contact.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { ContactCon, ContactFormCon, ContactIconsCon, ContactTitle, ContactTitleCon, IconButton } from "../components/contact/ContactElements";
 import { ContactForm } from "../components/contactForm/ContactForm";
-
+import gmailLogo from "../assets/gmailLogo.png"
 
 
 
@@ -18,16 +18,16 @@ export const Contact = () => {
             
           </ContactForm>
           <ContactIconsCon id="ContactIconsCon">
-             <Link to={'https://w.app/3YtU9p'} style={{textDecoration: 'none'}}>
-                <IconButton>X</IconButton>
-             </Link>
-             <Link to={'mailto:catalina.scheleff@gmail.com'} style={{textDecoration: 'none'}}>
+             <Link to={'https://w.app/3YtU9p'} target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none'}}>
                 <IconButton></IconButton>
              </Link>
-             <Link to={'https://w.app/3YtU9p'} style={{textDecoration: 'none'}}>
-                <IconButton></IconButton>
+             <Link to={'mailto:catalina.scheleff@gmail.com'} target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none'}}>
+                <IconButton src={gmailLogo}></IconButton>
              </Link>
-             <Link to={'https://w.app/3YtU9p'} style={{textDecoration: 'none'}}>
+             <Link to={'https://www.linkedin.com/in/catalina-scheleff-caceres/'} target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none'}}>
+                <IconButton ></IconButton>
+             </Link>
+             <Link to={'https://github.com/CatalinaScheleff'} target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none'}}>
                 <IconButton></IconButton>
              </Link>
             </ContactIconsCon>
