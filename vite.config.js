deeb686ddfc493997d94aa87f7bd061adbc9d35a
CatalinaScheleff <catascheleff@gmail.com>
2024-02-11 09:43:38 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  // Esta opción configura cómo Vite interpreta ciertos tipos de archivo.
-  // Asegúrate de que los archivos JSX se compilen a JavaScript estándar
-  // antes de ser servidos.
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-    // Asegura que los archivos JSX se compilen a JavaScript estándar
-    jsx: 'react'
-  }
 })
