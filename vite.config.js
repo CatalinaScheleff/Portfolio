652c1228088d9375fc1157f2042bd4f05c5326a0
CatalinaScheleff <catascheleff@gmail.com>
2024-02-11 17:01:08 -0300

    n

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  server: {
-    middleware: [
-      (req, res, next) => {
-        if (req.url.endsWith('.jsx')) {
-          res.setHeader('Content-Type', 'text/javascript');
-        }
-        next();
-      }
-    ]
-  }
 });
